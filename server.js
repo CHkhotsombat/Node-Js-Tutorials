5e4ac1cc8d30c4a5f694aaea4a9e3dfdc7bfe0a9
Chaiyasit.kh <chaiyasit.kh.57@gmail.com>
2022-01-17 20:11:57 +0700

    :sparkles: connect to MongoDB

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -7,8 +8,12 @@ const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
 const credentials = require('./middleware/credentials');
-
+const mongoose = require('mongoose');
+const connectDB = require('./config/dbConn');
 const PORT = process.env.PORT || 3500;
+
+// Connect to MongoDB
+connectDB();
 
 // custom middleware logger
 app.use(logger);
@@ -52,4 +57,7 @@ app.all('*', (req, res) => {
   }
 }) 
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB.');
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
